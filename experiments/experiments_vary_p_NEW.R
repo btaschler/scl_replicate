b4ddeddf715d26254e75f4798e4a375539cee89b
Bernd Taschler <bernd.taschler@ndcn.ox.ac.uk>
2021-03-31 21:40:47 +0100

    update: run on yeast data instead

--- experiments/experiments_vary_p_NEW.R
+++ experiments/experiments_vary_p_NEW.R
@@ -39,11 +39,15 @@ for(f in list.files(file.path(getwd(), "experiments", "methods") )){
 }
 source( file.path(getwd(), "experiments", "functions", "run_experiment.R") )
 require(R.utils)
-fid_data <- filePath(getwd(), 'data', 'shrna_processed_data.rds') 
+#fid_data <- filePath(getwd(), 'data', 'shrna_processed_data.rds')              #(A) human
+fid_data <- file.path(getwd(), 'data', 'yeast', 'yeast_processed_data.rds')     #(B) yeast
 data_list <- readRDS(fid_data)
 
 res_dir <- "/cluster/work/math/heinzec/scl/"
 data_dir <- "/cluster/work/math/heinzec/scl/"
+
+# res_dir <- file.path(getwd(), 'results', 'yeast')                               #TEMP
+# data_dir <- file.path(getwd(), 'data', 'yeast')
 # 
 
 ### Load functions
@@ -54,7 +58,8 @@ data_dir <- "/cluster/work/math/heinzec/scl/"
 
 set.seed(0)
 
-### Data: shrna
+
+### Data
 
 #data_list   = get_shrna_data()
 int_data    = data_list$int_data
@@ -65,29 +70,47 @@ obs_data    = data_list$obs_data
 
 ### Experiment settings ---------------------------------------------------
 
-# Data
-int_thresh_count = 35
-int_graph_count  = 35
-int_sample_count = 35
+# # (A) Data - setup for human (shrna) data
+# int_thresh_count = 35
+# int_graph_count  = 35
+# int_sample_count = 35
+# 
+# obs_thresh_count = NULL
+# obs_sample_count = 19
+# 
+# thresholding     = "max_min"      #robust_zscore_5
+# sparsity_factor  = 2
+# data_name        = "shrna"
+# remove_near_constant_columns = TRUE
 
-obs_thresh_count = NULL
-obs_sample_count = 19
 
-thresholding     = "max_min"
-sparsity_factor  = 2
-data_name        = "shrna"
+# (B) Data - setup for yeast data
+int_thresh_count = NULL
+int_graph_count  = 700
+int_sample_count = 700
+
+obs_thresh_count = 80
+obs_sample_count = 80
+
+thresholding     = "robust_zscore_5"      #robust_zscore_3 / .._5 / .._7
+sparsity_factor  = NULL
+data_name        = "yeast"
 remove_near_constant_columns = TRUE
+
 
 # Masking
 percentage_visible = 50
 mask_seq = c("rows", "entries")
 
-# Repetitions
-rep_seq = 1:10                                                                ## TESTING
+## Repetitions
+rep_seq = 1:10
 
-# Methods
 
-# p_seq = c(200) #c(25, 50, 100, 200, 500, 1000)                               ## TESTING
+
+## Methods ---------------------------------------------------------------
+
+#p_seq = c(25, 50, 100, 200, 500, 1000, 2000, 5000, 10000, Inf)                 #(A) human
+#p_seq = c(25, 50, 100, 200, 500, 1000, 2000, 4000, Inf)                        #(B) yeast 
 
 # method_p_list = list("method_cad_lasso" = p_seq,
 #                      "method_cad_keras" = p_seq)
@@ -98,7 +121,7 @@ method_seq = names(method_p_list)
 
 ### Generate datasets -------------------------------------------------------
 
-db_vary_p_path = file.path(data_dir, "data", "vary_p")
+db_vary_p_path = file.path(data_dir, "sampled_data", "vary_p")
 if(!dir.exists(db_vary_p_path)){ dbCreate(db_vary_p_path, type = "RDS") }
 
 db_vary_p = filehash::dbInit(db_vary_p_path, type = "RDS")
